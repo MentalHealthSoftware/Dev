2dc64ef8047f4e41cebcb31d822f04b5e0cfa558
Rushabh528 <rushabh2825@gmail.com>
2024-07-01 04:34:41 +0530

    Firebase initialized

---
+++ lib/login_page.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:mentalheathstartup/constants/color.dart';
+import 'package:mentalheathstartup/otp_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginState();
+}
+
+class _LoginState extends State<LoginScreen> {
+  TextEditingController countryController = TextEditingController();
+
+  @override
+  void initState() {
+    countryController.text = "+91";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.darkBlue, // Background color
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 60),
+            decoration: BoxDecoration(
+              color: AppColor.opaqueBlue, // Container color
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CircleAvatar(
+                  radius: 40,
+                  backgroundColor: Colors.white,
+                  child: Text('logo'),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Create an account',
+                  style: TextStyle(
+                    color: AppColor.darkBlue,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Enter your mobile number to sign up/login',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: AppColor.darkBlue,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  height: 55,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 1, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: 40,
+                        child: TextField(
+                          controller: countryController,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                      const Text(
+                        "|",
+                        style: TextStyle(fontSize: 33, color: Colors.grey),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Expanded(
+                          child: TextField(
+                        keyboardType: TextInputType.phone,
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Enter Mobile Number",
+                        ),
+                      ))
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // Handle OTP generation
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => OTPScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.darkOrange,
+                    foregroundColor: Colors.white, // Button color
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Get OTP',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/otp_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:mentalheathstartup/home_screen.dart';
+import 'package:pinput/pinput.dart';
+import 'package:mentalheathstartup/constants/color.dart';
+
+class OTPScreen extends StatefulWidget {
+  const OTPScreen({super.key});
+
+  @override
+  State<OTPScreen> createState() => _OTPState();
+}
+
+class _OTPState extends State<OTPScreen> {
+  TextEditingController countryController = TextEditingController();
+
+  @override
+  void initState() {
+    countryController.text = "+91";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(
+          fontSize: 20,
+          backgroundColor: Colors.white,
+          // color: Colors.white,
+          fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color.fromARGB(255, 198, 223, 223)),
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+      ),
+    );
+    return Scaffold(
+      backgroundColor: AppColor.darkBlue, // Background color
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            Row(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      // color: Colors.white,
+                      border: Border.all(color: AppColor.darkGreen, width: 1)),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back,
+                        color: AppColor.darkGreen, size: 30),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 60),
+                  decoration: BoxDecoration(
+                    color: AppColor.opaqueBlue, // Container color
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      SizedBox(height: 20),
+                      Text(
+                        'OTP Verification',
+                        style: TextStyle(
+                          color: AppColor.darkBlue,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            'Enter the OTP sent to ',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: AppColor.darkBlue,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            "+91 xxxxxx068",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: AppColor.darkBlue,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Pinput(
+                        length: 4,
+                        defaultPinTheme: defaultPinTheme,
+                        keyboardType: TextInputType.number,
+                        showCursor: true,
+                        onChanged: (pin) {
+                          print(pin);
+                        },
+                      ),
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Didn't Receive the OTP?",
+                            style: TextStyle(
+                              color: AppColor.darkBlue,
+                              fontSize: 14,
+                            ),
+                          ),
+                          Text(
+                            " RESEND OTP",
+                            style: TextStyle(
+                              color: AppColor.darkGreen,
+                              fontSize: 14,
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: () {
+                          // Handle OTP verification
+                          Navigator.pushNamed(context, "/home");
+                        },
+                        style: ElevatedButton.styleFrom(
+                          foregroundColor: Colors.white,
+                          backgroundColor: AppColor.darkOrange,
+                          // primary: Color(0xFFD4802A), // Button color
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          padding: EdgeInsets.symmetric(vertical: 16),
+                        ),
+                        child: Center(
+                          child: Text(
+                            'VERIFY & PROCEED',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
